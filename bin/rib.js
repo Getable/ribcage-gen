8526ed76d5827377f993736396b293f14aaa2528
Joey Baker <joey@byjoeybaker.com>
2016-01-20 16:17:37 -0800

    fix: Don’t override current directories

--- bin/rib.js
+++ bin/rib.js
@@ -3,14 +3,25 @@
 
 var gen = require('../index')
   , argv = require('minimist')(process.argv.slice(2))
-  , type = argv.t || argv.type || 'backbone'
+  , fs = require('fs')
+  , type = argv.t || argv.type || 'react'
+  , target = argv._[0]
 
-gen({target: argv._[0], type: type}, function (context) {
-  var msg = ''
-  if (context.exists) {
-    msg = 'Directory for "' + context.name + '" already exists! Starting demo instead :)'
-  }else {
-    msg = 'Created the ' + context.PascalName + ' component'
+fs.stat(target, function (err, stat) {
+  // on error, assume the directory doesn't exist, and we can proceed
+  if (err) {
+    gen({target: target, type: type}, function (context) {
+      var msg = ''
+      if (context.exists) {
+        msg = 'Directory for "' + context.name + '" already exists! Starting demo instead :)'
+      }else {
+        msg = 'Created the ' + context.PascalName + ' component'
+      }
+      console.info(msg)
+    })
   }
-  console.log(msg)
+  // else, assume the directory exists and we don't want to overwrite
+  else {
+    console.error('Whoops! That directory already exists, not overwriting.')
+  }
 })
